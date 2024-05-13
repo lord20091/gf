12c0d8c3e2fafb468dbac29f8846bd929296ef47
lord20091 <abdulazizbozorov009@gmail.com>
2024-05-13 18:09:31 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let son = Math.round(Math.random()*100)
+
+let text = document.getElementById('ism')
+
+let body = document.querySelector(`body`)
+
+text.innerHTML = son
+
+body.style.backgroundColor = `black`
+text.style.color = `red`
+text.style.display = `flex`
+text.style.justifyContent = `center`
+text.style.alignItems = `center`
+text.style.height = `100vh`
+text.style.fontSize = `150px`
+
+
+
+
+
+
